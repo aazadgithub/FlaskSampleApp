09de2158ae943555f69b4ab2d412cb5a88c8c2a7
Azad <aazadbabu.ss@gmail.com>
2020-09-14 23:00:45 +0530

    added new post method create stores

--- flask_sampleapp/app.py
+++ flask_sampleapp/app.py
@@ -23,4 +23,17 @@ def home():
 def get_stores():
     return jsonify({"stores":stores})
 
+
+@app.route('/store', methods=["post"])
+def create_store():
+    req_data = request.get_json()
+    new_store = {
+        "name": req_data["name"],
+        "items": [{
+            "name": "new item",
+            "price": 140
+        }]
+    }
+    return jsonify(new_store)
+
 app.run(port=5000)
